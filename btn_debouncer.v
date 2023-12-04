c61998d501c1468718627551056db2bce439b7ec
SharpPlant <43187312+SharpPig@users.noreply.github.com>
2023-12-03 18:49:44 -0600

    Add files via upload

---
+++ btn_debouncer.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+/*****************************************************
+
+*****************************************************/
+
+module btn_debouncer(
+    input clk_in,
+    input btn_in,
+    output btn_out
+    );
+    
+    reg t0, t1, t2;
+    
+    always @(posedge clk_in) begin
+        t0 <= btn_in;
+        t1 <= t0;
+        t2 <= t1;
+    end
+    
+    assign btn_out = t2;
+    
+endmodule
