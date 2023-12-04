4e4ea81fbc245195632b3181176664b62ef36a08
SharpPlant <43187312+SharpPig@users.noreply.github.com>
2023-12-03 19:00:07 -0600

    Update byte_calculator.v

--- byte_calculator.v
+++ byte_calculator.v
@@ -1,6 +1,17 @@
 `timescale 1ns / 1ps
 /*****************************************************
+byte_calculator.v
 
+Description:
+Verilog module for a byte calculator on BASYS 3 FPGA. It performs mathematical operations on two 8-bit values based on the selected operation. The result is stored in a 9-bit register.
+
+Inputs:
+- clk: Clock input
+- byte_A: 8-bit operand A
+- byte_B: 8-bit operand B
+- op_select: 5-bit operation selection
+Outputs:
+- result: 9-bit result of the operation
 *****************************************************/
 
 module byte_calculator(
